3fc2f4ad9316fc79ccf67a3a48e48ef0912fc31a
amilonurb <brunolima.18@outlook.com>
2019-06-02 22:44:51 -0300

    ajustes na rotina de gauss e lu

--- sistemas/cpp/decomposicao_lu.cpp
+++ sistemas/cpp/decomposicao_lu.cpp
@@ -2,14 +2,18 @@
 
 using namespace std;
 
-typedef vector<vector<double>> vvd;
+typedef vector<double> vd;
+typedef vector<vd> vvd;
 
-#define loop(init_var, init_value, range) for (init_var = init_value; init_var < range; init_var++)
-#define loop_eq(init_var, init_value, range) for (init_var = init_value; init_var <= range; init_var++)
+#define loop(init_var, init_value, range) for (int init_var = (init_value); init_var < (range); init_var++)
+#define loop_eq(init_var, init_value, range) for (int init_var = (init_value); init_var <= (range); init_var++)
 
-void print_matrix(const vvd &matriz, const string &title) {
+void print_matriz(vvd&, string&);
+double sum(vvd&, vvd&, int, int);
+void lu(vvd&);
+
+void print_matriz(vvd &matriz, const string &title) {
     cout << title << '\n';
-    int i, j;
     loop(i, 0, matriz[0].size()) {
         loop(j, 0, matriz[i].size()) {
             cout << matriz[i][j] << '\t';
@@ -18,22 +22,20 @@ void print_matrix(const vvd &matriz, const string &title) {
     }
 }
 
-double sum(const vvd &a, const vvd &b, int j, int i) {
+double sum(vvd &a, vvd &b, int j, int i) {
     double sum = 0.0;
-    int k;
     loop(k, 0, i) {
         sum += a[k][j] * b[i][k];
     }
     return sum;
 }
 
-void lu(const vvd &matriz) {
-    int n = matriz[0].size(), i, j;
-
+void lu(vvd &matriz) {
+    int n = matriz[0].size();
     vvd lower = vvd(n, vector<double>(n));
     vvd upper = vvd(n, vector<double>(n));
 
-    print_matrix(matriz, "Matriz A");
+    print_matriz(matriz, "Matriz A");
 
     loop(j, 0, n) {
         lower[j][j] = 1;
@@ -45,52 +47,10 @@ void lu(const vvd &matriz) {
             lower[i][j] = (matriz[i][j] - sum(upper, lower, j, i)) / upper[j][j];
         }
     }
-    
-    cout << '\n';
-    print_matrix(lower, "Matriz L");
 
     cout << '\n';
-    print_matrix(upper, "Matriz U");
-}
+    print_matriz(lower, "Matriz L");
 
-int main() {
-    /*int n, i, j;
-    cin >> n;
-
-    cout << "Matrix Dimension: " << n << " x " << n << '\n';
-
-    vvd matriz;
-    loop(i, 0, n) {
-        matriz.push_back(vector<double>(n));
-        loop(j, 0, n) {
-            double a;
-            cin >> a;
-            matriz[i][j] = a;
-        }
-    }
-
-    lu(matriz);*/
-    
-    // Lista 7
-    vvd ex1 = {{5.0, 2.0, 1.0}, {-1.0, 4.0, 2.0}, {2.0, -3.0, 10.0}};
-    vvd ex2 = {{2.0, 3.0, -1.0}, {1.0, 0.0, 2.0}, {0.0, 3.0, -1.0}};
-    vvd ex3 = {{10.0, 7.0, 8.0}, {7.0, 5.0, 6.0}, {8.0, 6.0, 10.0}};
-    vvd ex4 = {{2.0, 5.0, 3.0}, {5.0, 2.0, 1.0}, {1.0, 3.0, 6.0}};
-    vvd ex5 = {{8.0, -4.0, -2.0}, {-4.0, 10.0, -2.0}, {-2.0, -2.0, 10.0}};
-
-    lu(ex1);
-    cout << "\n\n";
-
-    lu(ex2);
-    cout << "\n\n";
-
-    lu(ex3);
-    cout << "\n\n";
-
-    lu(ex4);
-    cout << "\n\n";
-
-    lu(ex5);
-    cout << "\n\n";
-    return 0;
+    cout << '\n';
+    print_matriz(upper, "Matriz U");
 }
--- sistemas/cpp/eliminacao_gauss.cpp
+++ sistemas/cpp/eliminacao_gauss.cpp
@@ -3,17 +3,17 @@
 using namespace std;
 
 typedef vector<double> vd;
-typedef vector<vector<double>> vvd;
+typedef vector<vd> vvd;
 
-#define each(element, collection) for (auto &element : collection)
 #define loop(init_var, init_value, range) for (int init_var = (init_value); init_var < (range); init_var++)
-#define loop_rv(init_var, init_value, range) for (int init_var = (init_value); init_var >= (range); init_var--)
 #define loop_eq(init_var, init_value, range) for (int init_var = (init_value); init_var <= (range); init_var++)
+#define loop_rv(init_var, init_value, range) for (int init_var = (init_value); init_var >= (range); init_var--)
+#define each(element, collection) for (auto &element : collection)
 
-void print_vector(const vd &vetor, const string &title);
-void print_matrix(const vvd &matriz, const string &title);
-void solveGauss(vvd &a, vd &b);
-void solveLinearSystem(const vvd &a, const vd &b);
+void print_vetor(vd&, string&);
+void print_matriz(vvd&, string&);
+void solveGauss(vvd&, vd&);
+void solveLinearSystem(vvd&, vd&);
 
 int main() {
     vvd a = {
@@ -28,15 +28,15 @@ int main() {
     return 0;
 }
 
-void print_vector(const vd &vetor, const string &title) {
+void print_vetor(vd &vetor, const string &title) {
     cout << title << '\n';
     each(v, vetor) {
-        cout << v << '\t'
+        cout << v << '\t';
     }
     cout << endl;
 }
 
-void print_matrix(const vvd &matriz, const string &title) {
+void print_matriz(vvd &matriz, const string &title) {
     cout << title << '\n';
     loop(i, 0, matriz[0].size()) {
         loop(j, 0, matriz[i].size()) {
@@ -49,6 +49,44 @@ void print_matrix(const vvd &matriz, const string &title) {
 void solveGauss(vvd &a, vd &b) {
     int n = b.size();
     loop(k, 0, n - 1) {
+        /**
+         * Checa se é preciso realizar troca de linhas.
+         * Isso é necessário quando o pivô corrente é nulo.
+         * 
+         * Caso positivo, tenta-se fazer uma técnica chamada de pivotamento parcial.
+         */
+        if (a[k][k] == 0.0) {
+            /**
+             * Pivotamento Parcial
+             * 
+             * Seja p o menor inteiro, tal que |a[p][n]| =     max |a[j][k]|
+             *                                             k <= j <= n
+             * e k <= p <= n.
+             * 
+             * Se a[p][k] = 0, não existe solução única para o sistema linear.
+             * 
+             * Se p <> k, troca-se as linhas a[k] e a[p].
+             */
+            int p = k;
+            int a_max = fabs(a[p][k]);
+            loop(j, k, n) {
+                if (fabs(a[j][k]) > a_max) {
+                    a_max = fabs(a[j][k]);
+                    p = j;
+                }
+            }
+            
+            if (a[p][k] == 0.0) {
+                cout << "Sistema com muitas solucoes ou nao tem solucao\n";
+                return;
+            }
+
+            if (p != k) {
+                swap(a[p], a[k]);
+                swap(b[p], b[k]);
+            }            
+        }
+
         loop(i, k + 1, n) {
             double m = a[i][k] / a[k][k];
             loop(j, k, n) {
@@ -60,20 +98,30 @@ void solveGauss(vvd &a, vd &b) {
     solveLinearSystem(a, b);
 }
 
-void solveLinearSystem(const vvd &a, const vd &b) {
+void solveLinearSystem(vvd &a, vd &b) {
     int n = b.size();
-    vd x = vd(n, INT_MIN);
+    vd x = vd(n);
     x[n - 1] = b[n - 1] / a[n - 1][n - 1];
+    
+    if (a[n - 1][n - 1] == 0.0) {
+        cout << "\nSistema com muitas solucoes ou nao tem solucao\n";
+        return;
+    }
+
     loop_rv(i, n - 1, 0) {
         double sum = 0.0;
         loop(j, i + 1, n) {
             sum += a[i][j] * x[j];
         }
         x[i] = (b[i] - sum) / a[i][i];
+        if (a[i][i] == 0.0) {
+            cout << "\nSistema com muitas solucoes ou nao tem solucao\n";
+            return;
+        }
     }
 
-    cout << "Solucao do Sistema\n";
-    for (int i = 0; i < x.size(); i++) {
+    cout << "\nSolucao do Sistema\n";
+    loop(i, 0, x.size()) {
         cout << "X[" << (i + 1) << "] = " << x[i] << '\t';
     }
     cout << endl;
